9074c8086985241625c4684fe3e10e8756ab4db1
Ryan Chandler <ryan@chandler.codes>
2020-02-17 18:24:55 +0000

    Started creating Shortlink generation for application routes

---
+++ src/PendingShortlinkCreator.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace RyanChandler\Shortlinks;
+
+class PendingShortlinkCreator
+{
+    /**
+     * The URL that this shortlink represents.
+     * 
+     * @var string
+     */
+    protected $url;
+
+    /**
+     * Create a new PendingShortlinkCreator instance.
+     * 
+     * @param  string  $url
+     * @return void
+     */
+    public function __construct(string $url)
+    {
+        $this->url = $url;
+    }
+}
--- src/Shortlinks.php
+++ src/Shortlinks.php
@@ -2,7 +2,38 @@
 
 namespace RyanChandler\Shortlinks;
 
+use Illuminate\Config\Repository;
+
 class Shortlinks
 {
-    //
+    /**
+     * The shortlinks configurations.
+     * 
+     * @var array
+     */
+    protected $config;
+    
+    /**
+     * Create a new Shortlinks instance.
+     * 
+     * @param  \Illuminate\Config\Repository  $config
+     * @return void
+     */
+    public function __construct(Repository $config)
+    {
+        $this->config = $config->get('shortlinks');
+    }
+
+    /**
+     * Create a shortlink for a route.
+     * 
+     * @param  string  $route
+     * @param  array  $parameters
+     * @param  bool  $absolute
+     * @return \RyanChandler\Shortlinks\PendingShortlinkCreator
+     */
+    public function route(string $route, array $parameters = [], bool $absolute = true): PendingShortlinkCreator
+    {
+        return new PendingShortlinkCreator(route($route, $parameters, $absolute));
+    }
 }

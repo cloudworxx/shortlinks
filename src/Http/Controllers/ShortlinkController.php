55c45834b2b4b377e7f976a200f4d055a5cc1cc0
Ryan Chandler <ryan@chandler.codes>
2020-02-17 21:40:24 +0000

    Wrote controller handler

--- src/Http/Controllers/ShortlinkController.php
+++ src/Http/Controllers/ShortlinkController.php
@@ -9,6 +9,24 @@ class ShortlinkController
 {
     public function __invoke(Request $request, Shortlink $shortlink)
     {
+        if ($request->input('track_clicks') || $shortlink->track_clicks) {
+            $shortlink->increment('clicks');
+        }
+
+        $tracking = [];
+
+        if ($request->input('track_ip') || $shortlink->track_ip) {
+            $tracking['ip_address'] = $request->ip();
+        }
+
+        if ($request->input('track_agent') || $shortlink->track_agent) {
+            $tracking['agent'] = $request->userAgent();
+        }
+
+        if (!empty($tracking)) {
+            $shortlink->tracking()->create($tracking);
+        }
         
+        return redirect()->to($shortlink->destination);
     }
 }

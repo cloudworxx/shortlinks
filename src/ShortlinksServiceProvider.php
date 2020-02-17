324382e78bd827930bd0076447c33edb3e761f12
Ryan Chandler <ryan@chandler.codes>
2020-02-17 18:13:20 +0000

    Finished setting up Service Provider

--- src/ShortlinksServiceProvider.php
+++ src/ShortlinksServiceProvider.php
@@ -7,12 +7,29 @@ use Illuminate\Support\ServiceProvider;
 class ShortlinksServiceProvider extends ServiceProvider
 {
     /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->publishes([
+            __DIR__.'/../config/shortlinks.php' => config_path('shortlinks.php'),
+        ], 'config');
+
+        $this->loadRoutesFrom(__DIR__.'/../routes/web.php');
+        $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
+    }
+
+    /**
      * Register any application services.
      *
      * @return void
      */
     public function register()
     {
+        $this->mergeConfigFrom(__DIR__.'/../config/shortlinks.php', 'shortlinks');
+
         $this->app->singleton(Shortlinks::class, Shortlinks::class);
     }
 }

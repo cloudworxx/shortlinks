38f05282e9602455e18091d03ce2cee632253f17
Ryan Chandler <ryan@chandler.codes>
2020-02-18 18:02:20 +0000

    Added some comments to the config file

--- config/shortlinks.php
+++ config/shortlinks.php
@@ -2,11 +2,60 @@
 
 return [
 
+    /*
+    |--------------------------------------------------------------------------
+    | Table Prefix
+    |--------------------------------------------------------------------------
+    |
+    | This will be used to prefix all of the tables created by shortlinks.
+    |
+    | You should only change this if you already have table named similar
+    | to the ones generated here, but the chances of that are very slim.
+    |
+    */
+
     'database_table_prefix' => env('SHORTLINKS_TABLE_PREFIX', 'shortlinks_'),
+
+    /*
+    |--------------------------------------------------------------------------
+    | URL Prefix
+    |--------------------------------------------------------------------------
+    |
+    | This will be used to prefix all generated shortlinks, so that it can be
+    | distinguished from regular routes in your application.
+    |
+    | e.g. /s/shortlink
+    |
+    */
 
     'url_prefix' => env('SHORTLINKS_URL_PREFIX', 's'),
 
+    /*
+    |--------------------------------------------------------------------------
+    | Length
+    |--------------------------------------------------------------------------
+    |
+    | This determines the length of the shortlink string generated. Obviously,
+    | it's a shortlink so I wouldn't recommend going above 7 or 8.
+    |
+    | e.g. /s/12345678
+    |
+    */
+
     'length' => env('SHORTLINKS_LENGTH', 8),
+
+    /*
+    |--------------------------------------------------------------------------
+    | Tracking
+    |--------------------------------------------------------------------------
+    |
+    | This package provides some very basic tracking features so you can
+    | gather stats from your shortlinks.
+    |
+    | Supported: true (enables all tracking stats), or default array provides
+    |            more fine grained control over tracking.
+    |
+    */
 
     'tracking' => [
         'track_clicks' => true,
